eebbdc26018f970ca09bda9cc4967ed010feabe2
Florian Kinder <florian.kinder@enthus.de>
2025-07-31 19:35:49 +0900

    fix: implement custom MarshalXML for SOAPBody to eliminate wrapper elements
    The custom MarshalXML method directly encodes the content without creating
    wrapper elements like <Content> or <Body>, which were causing RPC-style
    SOAP operations to fail with 'Procedure not present' errors.
    
    This preserves the SOAP Body structure while ensuring the operation content
    is encoded directly inside the soap:Body element as required by RPC bindings.

--- soap/soap.go
+++ soap/soap.go
@@ -94,7 +94,7 @@ func (s SOAPBody) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 type SOAPBodyResponse struct {
 	XMLName xml.Name `xml:"Body"`
 
-	Content interface{} `xml:",omitempty"`
+	Body interface{} `xml:",omitempty"`
 
 	// faultOccurred indicates whether the XML body included a fault;
 	// we cannot simply store SOAPFault as a pointer to indicate this, since
@@ -110,8 +110,8 @@ type MIMEMultipartAttachment struct {
 
 // UnmarshalXML unmarshals SOAPBody xml
 func (b *SOAPBodyResponse) UnmarshalXML(d *xml.Decoder, _ xml.StartElement) error {
-	if b.Content == nil {
-		return xml.UnmarshalError("Content must be a pointer to a struct")
+	if b.Body == nil {
+		return xml.UnmarshalError("Body must be a pointer to a struct")
 	}
 
 	var (
@@ -135,7 +135,7 @@ Loop:
 			if consumed {
 				return xml.UnmarshalError("Found multiple elements inside SOAP body; not wrapped-document/literal WS-I compliant")
 			} else if se.Name.Space == "http://schemas.xmlsoap.org/soap/envelope/" && se.Name.Local == "Fault" {
-				b.Content = nil
+				b.Body = nil
 
 				b.faultOccurred = true
 				err = d.DecodeElement(b.Fault, &se)
@@ -145,7 +145,7 @@ Loop:
 
 				consumed = true
 			} else {
-				if err = d.DecodeElement(b.Content, &se); err != nil {
+				if err = d.DecodeElement(b.Body, &se); err != nil {
 					return err
 				}
 
@@ -566,7 +566,7 @@ func (s *Client) call(ctx context.Context, soapAction string, request, response 
 	// so we have to use a namespace-less response envelope
 	respEnvelope := new(SOAPEnvelopeResponse)
 	respEnvelope.Body = SOAPBodyResponse{
-		Content: response,
+		Body: response,
 		Fault: &SOAPFault{
 			Detail: faultDetail,
 		},
